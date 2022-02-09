9bec2f05d744465b5e1a266a56257d5698465e1c
pgpratheesh <pgpratheesh@gmail.com>
2022-02-09 19:06:11 +0530

    Update slack.sh

--- slack.sh
+++ slack.sh
@@ -4,4 +4,4 @@ message=$1
  
 [ ! -z "$message" ] && curl -X POST -H 'Content-type: application/json' --data "{
               \"text\": \"${message}\"
-      }" https://hooks.slack.com/services/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
+      }" https://hooks.slack.com/services/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
