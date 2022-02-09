110b54190d874ba3615cbe055ab8aab34f4dfc7c
pgpratheesh <pgpratheesh@gmail.com>
2022-02-09 17:56:16 +0530

    Create slack.sh

---
+++ slack.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+ 
+message=$1
+ 
+[ ! -z "$message" ] && curl -X POST -H 'Content-type: application/json' --data "{
+              \"text\": \"${message}\"
+      }" https://hooks.slack.com/services/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
